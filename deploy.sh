73f6bdf0f8985880d2da000c707366f8709a3e1b
mkarim <107554278+MABD-KARIM@users.noreply.github.com>
2023-09-01 17:39:51 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+set -e
+
+# Vos identifiants et configurations de GCP
+GCP_PROJECT_ID="id projet gcp"
+INSTANCE_NAME="nom d'instance du VM"
+ZONE="zone"
+
+# Copier les fichiers sur la VM GCP
+gcloud compute scp --recurse $GITHUB_WORKSPACE/* $INSTANCE_NAME:~/ --project=$GCP_PROJECT_ID --zone=$ZONE
+
+# Exécuter des commandes sur la VM pour déployer l'application
+gcloud compute ssh $INSTANCE_NAME --project=$GCP_PROJECT_ID --zone=$ZONE --command="cd ~/ && pip install -r requirements.txt && python /src/main.py"
 
